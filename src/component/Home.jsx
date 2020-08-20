57311c259639851b5676370c0a140b68ba662975
menahem <mnjol770@gmail.com>
2020-08-20 17:37:19 +0300

    fill home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -11,6 +11,57 @@ export default function Home() {
         amet nostrum minus!
       </p>
       <hr />
+      <div className="grid">
+        <img
+          style={{ borderRadius: "50%", height: "150px", width: "150px" }}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTxPEK_NOOynvLPFdU_phc0q9PUunVDl7ijhQ&usqp=CAU"
+          alt="img"
+        />
+        <div className="grid-text">
+          <h1>Let's program </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+            accusantium dolor dolores, temporibus doloribus alias sequi iure
+            accusamus, autem quibusdam, repudiandae obcaecati quidem dolorum
+            quas veritatis hic voluptatibus error soluta.
+          </p>
+        </div>
+      </div>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Lastname</th>
+            <th>Lastname</th>
+          </tr>
+          <tr>
+            <td>Peter</td>
+            <td>Griffin</td>
+            <td>Griffin</td>
+            <td>Griffin</td>
+          </tr>
+          <tr>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Griffin</td>
+          </tr>
+          <tr>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Griffin</td>
+          </tr>
+          <tr>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Lois</td>
+            <td>Griffin</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
